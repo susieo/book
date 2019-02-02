25102a18d847a39422d68d4df9ee7ed0994957f8
susieo <susytuty@gmail.com>
2019-02-01 18:17:44 -0800

    tarea

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-axios-bookfinder
+
---
+++ src/components/SearchInput.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Well, FormControl } from 'react-bootstrap';
+
+class SearchInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    }
+  }
+
+  onChange(e) {
+    this.setState({value: e.target.value});
+    this.props.onChange(this.state.value);
+  }
+
+  render() {
+    return (
+      <div className="style">
+      <Well>
+        <h1> BookFinder</h1>
+        <FormControl
+          type="text"
+          value={this.state.value}
+          placeholder="Search Books..."
+          onChange={this.onChange.bind(this)}
+        />
+      </Well>
+      </div>
+    );
+  }
+}
+
+export default SearchInput;
